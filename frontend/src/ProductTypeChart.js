3cabe567432d63dea4504e48ce26ead1cff92fe1
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-06-06 15:33:30 +0200

    Update ProductTypeChart.js

---
+++ frontend/src/ProductTypeChart.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import Papa from "papaparse";
+import {Chart , registerables} from "chart.js";
+
+// Allowed extensions for input file
+const allowedExtensions = ["csv"];
