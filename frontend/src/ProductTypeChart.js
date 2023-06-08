d40ee19da9d499df2af8c00e7f0da9658e377c32
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-06-08 17:17:28 +0200

    ProductTypeChart.js pakt nu de juiste kolomnamen

--- frontend/src/ProductTypeChart.js
+++ frontend/src/ProductTypeChart.js
@@ -13,22 +13,43 @@ const ProductTypeChart = () => {
           Papa.parse(csvData, {
             header: true,
             complete: function (results) {
-              // Verwerk de gegevens hier volgens je behoeften
-              // Bijvoorbeeld, zet de gegevens om naar het juiste formaat voor Chart.js
-              
-              // Voorbeeld van gegevensverwerking:
-              const chartLabels = results.data.map(entry => entry.label);
-              const chartValues = results.data.map(entry => entry.value);
-              
-              setChartData({
-                labels: chartLabels,
-                datasets: [
-                  {
-                    label: 'Data',
-                    data: chartValues,
-                  },
-                ],
+              const data = results.data;
+              const chartData = {};
+              const labels = [];
+              const counts = {};
+      
+              data.forEach(entry => {
+                const year = entry['Year'];
+                const category = entry['prod_category'];
+      
+                // Voeg het jaar toe aan de labels-array als het nog niet bestaat
+                if (!labels.includes(year)) {
+                  labels.push(year);
+                }
+      
+                // Verhoog de telling voor de categorie in het betreffende jaar
+                if (!counts[year]) {
+                  counts[year] = {};
+                }
+                if (!counts[year][category]) {
+                  counts[year][category] = 0;
+                }
+                counts[year][category]++;
               });
+      
+              // Maak datasets voor elke categorie
+              const datasets = Object.keys(counts).map(category => {
+                const data = labels.map(year => counts[year][category] || 0);
+                return {
+                  label: category,
+                  data: data,
+                };
+              });
+      
+              chartData.labels = labels;
+              chartData.datasets = datasets;
+      
+              setChartData(chartData);
             },
           });
         }
